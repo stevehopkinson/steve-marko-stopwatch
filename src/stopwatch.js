c335187b22abf813620713e57534a2563d4187dd
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-10-26 17:08:32 +0100

    Add event listeners to buttons and added function to update DOM

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,25 +1,25 @@
-// var running;
-// var watch;
+var running;
+var watch;
 var timePassed;
 var prevTime;
 var currTime;
-//
-// var watchDisplay = document.getElementById('watchDisplay');
-// var buttons = document.querySelectorAll("button");
-// for (var i = 0; i < buttons.length; i++) {
-//   var button = buttons[i];
-//   button.onclick = function() {
-//     handleInput(this.id);
-//   };
-// };
+
+var watchDisplay = document.getElementById("watchDisplay");
+var buttons = document.querySelectorAll("button");
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.onclick = function() {
+    handleInput(this.id);
+  };
+};
 
 function initialise () {
-  // running = false;
-  // window.clearInterval(watch);
+  running = false;
+  window.clearInterval(watch);
   timePassed = 0;
   prevTime = 0;
   currTime = 0;
-  // update();
+  updateDOM();
 };
 
 function startTimer () {
@@ -38,10 +38,46 @@ function update () {
   var difference = currTime - prevTime;
   timePassed += difference;
   prevTime = currTime;
+  updateDOM();
 }
 
-function handleInput () {
-  // Handle JavaScript events
+function formatTime (time) {
+  var hours = Math.floor(time / 3600000);
+  time -= hours * 3600000;
+  var minutes = Math.floor(time / 60000);
+  time -= minutes * 60000;
+  var seconds = Math.floor(time / 1000);
+  time -= seconds * 1000;
+  var centiseconds = Math.floor(time/10);
+
+  while (hours.toString().length < 2) {
+    hours = "0" + hours;
+  }
+  while (minutes.toString().length < 2) {
+    minutes = "0" + minutes;
+  }
+  while (seconds.toString().length < 2) {
+    seconds = "0" + seconds;
+  }
+  while (centiseconds.toString().length < 2) {
+    centiseconds = "0" + centiseconds;
+  }
+  return `${hours}:${minutes}:${seconds}.${centiseconds}`;
+}
+
+function updateDOM () {
+  watchDisplay.textContent = formatTime(timePassed);
+}
+
+function handleInput (id) {
+  var inputs = {
+    startStop : function () {
+      running ? stopTimer() : startTimer();
+    },
+    reset : initialise
+  };
+
+  inputs[id]();
 }
 
 initialise();
