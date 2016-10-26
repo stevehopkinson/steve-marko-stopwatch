e18babc6953700116162ce915ff5d86496f4b144
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-10-26 12:47:04 +0100

    Add skeleton of functions

---
+++ src/stopwatch.js
@@ -0,0 +1,15 @@
+function initialise () {
+  // Set up start state
+};
+
+function startTimer () {
+  // Sets up timer
+}
+
+function update () {
+  // Updates current time
+}
+
+function handleInput () {
+  // Handle JavaScript events
+}
