afdccbaac650a9ac0da8226a1e1ceb61ac315d6d
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-10-27 17:24:14 +0100

    Refactor clock into a constructor function and add functionality for user to add multiple clocks

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,72 +1,119 @@
-var running, watch, timePassed, prevTime, currTime;
+var stopwatchContainer = document.getElementById("stopwatchContainer");
+var newStopwatchButton = document.getElementById("newStopwatchButton");
 
-var watchDisplay = document.getElementById("watchDisplay");
-var startStop = document.getElementById("startStop");
-var buttonText = document.getElementById("buttonText");
-var buttons = document.querySelectorAll("button");
-for (var i = 0; i < buttons.length; i++) {
-  var button = buttons[i];
-  button.addEventListener("click", function() {
-    handleInput(this.id);
+newStopwatchButton.onclick = createStopwatch;
+
+var stopwatches = [];
+var startIconClass = "fa fa-play-circle";
+var pauseIconClass = "fa fa-pause-circle";
+var stopIcon = '<i class="fa fa-stop-circle" aria-hidden="true"></i>';
+
+var Stopwatch = function () {
+  var running, watch, timePassed, prevTime, currTime;
+
+  var wrapper = document.createElement("DIV");
+  wrapper.setAttribute("class", "stopwatch__wrapper col-4");
+
+  var section = document.createElement("SECTION");
+  section.setAttribute("class", "stopwatch");
+  wrapper.appendChild(section);
+
+  var name = document.createElement("H2");
+  name.setAttribute("class", "stopwatch__name");
+  name.textContent = "Stopwatch " + (stopwatches.length + 1);
+  section.appendChild(name);
+
+  var display = document.createElement("P");
+  display.setAttribute("class", "stopwatch__display");
+  section.appendChild(display);
+
+  var startStop = document.createElement("BUTTON");
+  startStop.setAttribute("class", "stopwatch__button");
+  section.appendChild(startStop);
+
+  var startStopIcon = document.createElement("I");
+  startStopIcon.setAttribute("class", "fa fa-play-circle");
+  startStopIcon.setAttribute("aria-hidden", "true");
+  startStop.appendChild(startStopIcon);
+
+  var reset = document.createElement("BUTTON");
+  reset.setAttribute("class", "stopwatch__button");
+  reset.innerHTML = stopIcon;
+  section.appendChild(reset);
+
+  startStop.addEventListener("click", function() {
+      handleInput("startStop");
   });
-};
 
-var startIcon = '<i class="fa fa-play-circle" aria-hidden="true"></i>';
-var pauseIcon = '<i class="fa fa-pause-circle" aria-hidden="true"></i>';
+  reset.addEventListener("click", function() {
+      handleInput("reset");
+  });
 
-function initialise () {
-  running = false;
-  window.clearInterval(watch);
-  timePassed = 0;
-  prevTime = 0;
-  currTime = 0;
-  updateDOM();
-};
-
-function startTimer () {
-  running = true;
-  prevTime = (new Date).getTime();
-  watch = window.setInterval(update, 11);
-  update();
-}
-
-function stopTimer () {
-  running = false;
-  window.clearInterval(watch);
-  update();
-}
-
-function update () {
-  currTime = (new Date).getTime();
-  var difference = currTime - prevTime;
-  timePassed += difference;
-  prevTime = currTime;
-  updateDOM();
-}
-
-function formatTime (time) {
-  var centiseconds = Math.floor((time / 10) % 100);
-  var seconds = Math.floor((time / 1000) % 60);
-  var minutes = Math.floor((time / (60 * 1000)) % 60);
-  var hours = Math.floor((time / (60 * 60 * 1000)) % 24);
-
-  return `${hours}:${minutes}:${seconds}.${centiseconds}`.replace(/(^|\:|\.)(\d)(?!\d)/g, (x,a,b) => a+'0'+b);
-}
-
-function updateDOM () {
-  watchDisplay.textContent = formatTime(timePassed);
-  buttonText.innerHTML = running ? pauseIcon : startIcon;
-}
-
-function handleInput (id) {
-  console.log(id);
-  var inputs = {
-    startStop : running ? stopTimer : startTimer,
-    reset : initialise
+  function initialise () {
+    running = false;
+    window.clearInterval(watch);
+    timePassed = 0;
+    prevTime = 0;
+    currTime = 0;
+    updateDOM();
   };
 
-  inputs[id]();
-  updateDOM();
-}
+  function startTimer () {
+    running = true;
+    prevTime = (new Date).getTime();
+    watch = window.setInterval(update, 11);
+    update();
+  }
 
-initialise();
+  function stopTimer () {
+    running = false;
+    window.clearInterval(watch);
+    update();
+  }
+
+  function update () {
+    currTime = (new Date).getTime();
+    var difference = currTime - prevTime;
+    timePassed += difference;
+    prevTime = currTime;
+    updateDOM();
+  }
+
+  function formatTime (time) {
+    var centiseconds = Math.floor((time / 10) % 100);
+    var seconds = Math.floor((time / 1000) % 60);
+    var minutes = Math.floor((time / (60 * 1000)) % 60);
+    var hours = Math.floor((time / (60 * 60 * 1000)) % 24);
+
+    return `${hours}:${minutes}:${seconds}.${centiseconds}`.replace(/(^|\:|\.)(\d)(?!\d)/g, (x,a,b) => a+'0'+b);
+  }
+
+  function updateDOM () {
+    display.textContent = formatTime(timePassed);
+    startStopIcon.setAttribute("class", running ? pauseIconClass : startIconClass);
+  }
+
+  function handleInput (id) {
+    var inputs = {
+      startStop : running ? stopTimer : startTimer,
+      reset : initialise
+    };
+
+    inputs[id]();
+    updateDOM();
+  }
+
+  this.getDOM = function () {
+    return wrapper;
+  }
+
+  initialise();
+};
+
+function createStopwatch() {
+  var stopwatch = new Stopwatch();
+  stopwatches.push(stopwatch);
+  stopwatchContainer.appendChild(stopwatch.getDOM());
+};
+
+createStopwatch();
